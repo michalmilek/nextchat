edce1ec4c37f9f864fa47c1107a2212ed71ff48f
michalmilek <mmilek1337@gmail.com>
2023-09-26 00:18:59 +0200

    feat(nextAuth); added credentials provider in next auth

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,68 @@
+import bcrypt from "bcrypt";
+import NextAuth, { AuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import GithubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import prisma from "@/app/libs/prismadb";
+import { User } from "../../../../../node_modules/next-auth/index";
+
+interface CustomUser extends User {
+  password: string;
+}
+
+export const authOptions: AuthOptions = {
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID as string,
+      clientSecret: process.env.GITHUB_SECRET as string,
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+    CredentialsProvider({
+      name: "credentials",
+      credentials: {
+        email: { label: "email", type: "text" },
+        password: { label: "password", type: "password" },
+      },
+      authorize: async (credentials: CustomUser): Promise<User> => {
+        if (!credentials?.email || !credentials?.password) {
+          throw new Error("Invalid credentials");
+        }
+
+        const user = await prisma.user.findUnique({
+          where: {
+            email: credentials.email,
+          },
+        });
+
+        if (!user || !user?.hashedPasssword) {
+          throw new Error("Invalid credentials");
+        }
+
+        const isCorrectPassword = await bcrypt.compare(
+          credentials.password,
+          user.hashedPasssword
+        );
+
+        if (!isCorrectPassword) {
+          throw new Error("Invalid credentials");
+        }
+
+        return user;
+      },
+    }),
+  ],
+  debug: process.env.NODE_ENV === "development",
+  session: {
+    strategy: "jwt",
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+};
+
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
---
+++ src/components/buttons/Divider.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import clsx from "clsx";
+
+interface DividerProps extends React.ComponentProps<"hr"> {
+  text: string;
+  className?: string;
+}
+
+const Divider: React.FC<DividerProps> = ({ text, className, ...rest }) => {
+  return (
+    <div className="relative">
+      <hr
+        className={clsx("border-t-2 border-gray-300", className)}
+        {...rest}
+      />
+      <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white px-2 whitespace-nowrap">
+        {text}
+      </div>
+    </div>
+  );
+};
+
+export default Divider;
